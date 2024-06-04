30c21bc39b7346a1e9980fcae0430f89c9da6902
botOliver <botoliver556@gmail.com>
2024-06-04 22:13:39 +0300

    first Test

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const emit = defineEmits(['some-click'])
+const counter = ref(0)
+
+defineProps({
+  msgTest: {
+    type: String,
+    default: 'HI'
+  }
+})
+
+function testFunc() {
+  emit('some-click', 'новый текст', counter.value++)
+}
+</script>
+
+<template>
+  <h1 @click="testFunc">{{ msgTest }} {{ counter }}</h1>
+</template>
+
+<style scoped></style>
---
+++ src/components/TestComponent2.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const emit = defineEmits(['test-click'])
+const counter = ref(0)
+
+defineProps({
+  newTest: {
+    type: String,
+    default: 'по дефолту'
+  }
+})
+
+function testFunc() {
+  emit('test-click', 'Проверено')
+}
+</script>
+
+<template>
+  <h1 @click="testFunc">{{ newTest }}</h1>
+</template>
+
+<style scoped></style>
